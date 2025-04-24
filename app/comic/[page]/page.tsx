6c04881da6086b4dc1372eeebf5d1f86157d31a3
walmik <walmik.deshpande@gmail.com>
2025-04-23 18:01:24 -0700

    Add images 18 to 23

--- app/comic/[page]/page.tsx
+++ app/comic/[page]/page.tsx
@@ -9,7 +9,7 @@ import { ChevronLeft, ChevronRight, Home } from "lucide-react"
 import { Pagination } from "@/components/comic-pagination"
 
 // Total number of pages in the comic
-const TOTAL_PAGES = 17
+const TOTAL_PAGES = 23
 
 export default function ComicPage() {
   const router = useRouter()
